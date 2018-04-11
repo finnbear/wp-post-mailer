e9300ddb30de0de3331a479742881a36e37d190c
finnbear <finnbearone@gmail.com>
2018-04-10 17:45:29 -0700

    First test.

---
+++ wp-post-mailer.php
@@ -0,0 +1,39 @@
+<?php
+   /*
+   Plugin Name: Post Mailer
+   Plugin URI: https://github.com/finnbear/wp-post-mailer
+   Version: 1.0
+   Author: Finn Bear
+   Author URI: https://www.finnbear.com
+   */
+
+   defined( 'ABSPATH' ) or die( 'Post Mailer not loaded by WordPress.' );
+
+   function post_mailer_activate() {
+
+   }
+
+   function post_mailer_deactivate() {
+
+   }
+
+   function post_mailer_uninstall() {
+
+   }
+
+   function post_mailer_form_shortcode() {
+    ob_start();
+
+    if ( isset( $_POST['submit'] ) ) {
+      <p>Post received.</p>
+    }
+
+    return ob_get_clean();
+   }
+
+   register_activation_hook(__FILE__, 'post_mailer_activate')
+   register_deactivation_hook(__FILE__, 'post_mailer_activate')
+   register_uninstall_hook(__FILE__, 'post_mailer_uninstall')
+
+   add_shortcode('post_mailer_form', 'post_mailer_form_shortcode')
+?>
